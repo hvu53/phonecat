01b240745e83c3ac2afcac6414fc75efe0cc2d14
Hoa VU <hoavt040789@gmail.com>
2014-03-20 15:33:05 -0700

    more templating

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,14 +4,18 @@
 
 var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('phones/phones.json').success(function(data) {
-        $scope.phones = data;
-    });
-    $scope.orderProp = "age";
-}]);
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+    function($scope, $http) {
+        $http.get('phones/phones.json').success(function(data) {
+            $scope.phones = data;
+        });
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-    function($scope, $routeParams) {
-        $scope.phoneId = $routeParams.phoneId;
+        $scope.orderProp = 'age';
+    }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+    function($scope, $routeParams, $http) {
+        $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+            $scope.phone = data;
+        });
     }]);
